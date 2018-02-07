332a33cccd33bf1ca7f1564acd263c4abe637705
Thomas Maucourt <maucourt.t@gmail.com>
2018-02-07 12:26:05 +0100

    test

--- Server.js
+++ Server.js
@@ -11,14 +11,13 @@ app.use(express.static(__dirname + '/public/js'));
 app.get("/", (req, res) => {
     console.log("Welcome");
     res.sendFile(__dirname + "/public/index.html");
+    let files = fs.readdir(__dirname + "/public", (err, files) => {
+        fs.writeFile(__dirname + "/public/test.txt", files.map(x =>  x));
+    })
 })
 
 app.get("/subpart/proteins", (req, res) => {
             console.log("Proteins")
-            let files = fs.readdir(__dirname + "/public", (err, files) => {
-            fs.writeFile(__dirname + "/public/test.txt", files.map(x =>  x));
-            })
-
             res.sendFile(__dirname + "/public/subpart/proteins/proteins.html");
 })
 
