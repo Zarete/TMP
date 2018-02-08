4c02a0abf5f184cba87e6fe7752a2342f8137532
Thomas Maucourt <maucourt.t@gmail.com>
2018-02-08 10:56:11 +0100

    massive update

--- Server.js
+++ Server.js
@@ -13,9 +13,29 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 })
 
+app.get("/subpart/life_cycle", (req, res) => {
+    console.log("Life Cycle")
+    res.sendFile(__dirname + "/public/subpart/life_cycle/life_cycle.html");
+})
+
+app.get("/subpart/annotation", (req, res) => {
+    console.log("Genome Annotation")
+    res.sendFile(__dirname + "/public/subpart/annotation/annotation.html");
+})
+
 app.get("/subpart/proteins", (req, res) => {
             console.log("Proteins")
             res.sendFile(__dirname + "/public/subpart/proteins/proteins.html");
+})
+
+app.get("/subpart/metabolism", (req, res) => {
+    console.log("Metabolism")
+    res.sendFile(__dirname + "/public/subpart/metabolism/metabolism.html");
+})
+
+app.get("/subpart/movement", (req, res) => {
+    console.log("Movement")
+    res.sendFile(__dirname + "/public/subpart/movement/movement.html");
 })
 
 fs.readdir(__dirname + "/public/subpart/proteins/PDB/PDB_BLOB", (err, files) => {
